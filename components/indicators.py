37b763e4eeea0ed48572ad9e6af3783aee773372
paul10116 <paulius.beinoras@gmail.com>
2023-02-06 11:10:17 +0200

    2023-02-06

--- components/indicators.py
+++ components/indicators.py
@@ -1,12 +1,14 @@
-import yfinance as yf
 import pandas as pd
 import numpy as np
+import sqlite3
+import sqlalchemy
+
+engine = sqlalchemy.create_engine('sqlite:///database.db')
+spy = pd.read_sql_query(
+    "SELECT * FROM indexes", engine, parse_dates="Date")
 
 
 def beta(data, ticker):
-
-    beta_ticker = yf.Ticker("SPY")
-    spy = beta_ticker.history(start='2020-1-1', actions=False, rounding=True)
 
     spy_ticker1 = pd.concat(
         [spy.Close[-500::], data[-500::]], axis=1)
@@ -16,13 +18,15 @@ def beta(data, ticker):
     ticker1_var = long_data_pct["SPY"].var()
     long_beta = ticker1_cov/ticker1_var
 
+    # beta_ticker = yf.Ticker("SPY")
+    # spy = beta_ticker.history(start='2020-1-1', actions=False, rounding=True)
+
+    # spy_ticker1 = pd.concat(
+    #     [spy.Close[-500::], data[-500::]], axis=1)
+    # spy_ticker1.columns = ["SPY", ticker]
+    # long_data_pct = np.log(spy_ticker1/spy_ticker1.shift())
+    # ticker1_cov = long_data_pct.cov().iloc[0, 1]
+    # ticker1_var = long_data_pct["SPY"].var()
+    # long_beta = ticker1_cov/ticker1_var
+
     return long_beta
-
-
-def vol_channel(data):
-    channel = pd.DataFrame({
-        "UpperBand": (data.rolling(50).mean()) +
-        (data.rolling(50).std()),
-        "LowerBand": (data.rolling(50).mean()) -
-        (data.rolling(50).std())
-    })
--- components/volatilityFinder.py
+++ components/volatilityFinder.py
@@ -1,5 +1,3 @@
-import yfinance as yf
-from datetime import date
 import pandas as pd
 from plotly import tools
 import plotly.graph_objs as go
@@ -8,7 +6,6 @@ import sqlite3
 import sqlalchemy
 from indicators import beta
 
-start_date = date.fromisoformat("2015-01-03")
 engine = sqlalchemy.create_engine('sqlite:///database.db')
 data = pd.read_sql_query(
     "SELECT * FROM stockData", engine, parse_dates="Date")
@@ -27,59 +24,80 @@ def pair(longs, shorts):
                 dataframe = pd.read_sql_query(
                     f"SELECT Date, {buy_ticker}, {sell_ticker} FROM stockData", engine, parse_dates="Date")
 
+                long_beta = beta(dataframe[buy_ticker], buy_ticker).round(2)
+                short_beta = beta(dataframe[sell_ticker], sell_ticker).round(2)
+                beta_bearish = long_beta < short_beta
+
                 corr_check = pd.concat(
                     [dataframe[buy_ticker], dataframe[sell_ticker]], axis=1).corr().iloc[0, 1].round(2)
 
                 print(
                     f"{buy_ticker} / {sell_ticker} CORR = {corr_check}")
+
                 if corr_check > 0.3 or corr_check < -0.3:
-                    data = pd.DataFrame({
-                        'ratio': (dataframe[buy_ticker]/dataframe[sell_ticker]).round(3),
-                        'spread': (dataframe[buy_ticker]-dataframe[sell_ticker]).round(2)
-                    })
+                    if beta_bearish == True:
 
-                    if data.iloc[-1, 0] > 0.4 or data.iloc[-1, 0] < 3:
-                        data['spreadMA200'] = data['spread'].rolling(
-                            200).mean().round(2)
-                        data['ratioMA200'] = data['ratio'].rolling(
-                            200).mean().round(2)
+                        data = pd.DataFrame({
+                            'ratio': (dataframe[buy_ticker]/dataframe[sell_ticker]),
+                            'spread': (dataframe[buy_ticker]-dataframe[sell_ticker]).round(2)
+                        })
 
-                        if data.iloc[-1, 0] > data.iloc[-1, 2] and data.iloc[-1, 1] > data.iloc[-1, 3]:
+                        ratio_lower = data.iloc[-1, 0] > 0.4
+                        ratio_upper = data.iloc[-1, 0] < 3
 
-                            data["spreadMA60"] = data.spread.rolling(
-                                60).mean().round(2)
-                            data["spreadMA20"] = data.spread.rolling(
-                                20).mean().round(2)
+                        if ratio_lower == True and ratio_upper == True:
 
-                            if data.iloc[-1, 5] < data.iloc[-1, 4]:
+                            data['spreadMA200'] = data.spread.ewm(
+                                span=200, adjust=False).mean()
+                            data["ratioEMA"] = data.ratio.ewm(
+                                span=200, adjust=False).mean()
+                            ratio_bullish = data.iloc[-1,
+                                                      3] > data.iloc[-60, 3]
+                            spread_bullish = data.iloc[-1,
+                                                       2] > data.iloc[-60, 2]
 
-                                sub_trace1 = go.Scatter(x=dataframe.Date,
-                                                        y=dataframe[buy_ticker], mode='lines', name=buy_ticker)
-                                sub_trace2 = go.Scatter(x=dataframe.Date,
-                                                        y=dataframe[sell_ticker], mode='lines', name=sell_ticker)
-                                sub_trace3 = go.Scatter(x=dataframe.Date,
-                                                        y=data.ratio, mode='lines', name='Ratio')
-                                sub_trace5 = go.Scatter(x=dataframe.Date,
-                                                        y=data.spread, mode='lines', name='Spread')
-                                sub_trace6 = go.Scatter(x=dataframe.Date,
-                                                        y=data.spreadMA200, mode='lines', name='Spread MA 200')
-                                sub_trace7 = go.Scatter(x=dataframe.Date,
-                                                        y=data.spreadMA60, mode='lines', name='Spread MA 60')
-                                sub_trace8 = go.Scatter(x=dataframe.Date,
-                                                        y=data.spreadMA20, mode='lines', name='Spread MA 20')
+                            if spread_bullish == True and ratio_bullish == True:
+                                data["ratioMin"] = data.ratio.rolling(60).min()
+                                data["ratioMax"] = data.ratio.rolling(60).max()
+                                data["ratioATR"] = data.ratioMax - \
+                                    data.ratioMin
+                                data["ratioATR%"] = (
+                                    (data.ratioATR+data.ratioATR.shift())/2)/data.ratioMax
 
-                                sub_fig = tools.make_subplots(rows=2, cols=2, subplot_titles=[
-                                    buy_ticker, 'Ratio', sell_ticker, "Spread"], shared_xaxes=True, x_title=f"Correlation: {corr_check}")
-                                sub_fig.append_trace(sub_trace1, 1, 1)
-                                sub_fig.append_trace(sub_trace2, 2, 1)
-                                sub_fig.append_trace(sub_trace3, 1, 2)
-                                sub_fig.append_trace(sub_trace5, 2, 2)
-                                sub_fig.append_trace(sub_trace6, 2, 2)
-                                sub_fig.append_trace(sub_trace7, 2, 2)
-                                sub_fig.append_trace(sub_trace8, 2, 2)
+                                ratio_ATR = data.iloc[-1, 7] > 0.3
 
-                                sub_fig.show()
-                                print(data.tail(2))
+                                if ratio_ATR == True:
+                                    print(data.tail(1))
+
+                                    sub_trace1 = go.Scatter(x=dataframe.Date,
+                                                            y=dataframe[buy_ticker], mode='lines', name=buy_ticker)
+                                    sub_trace2 = go.Scatter(x=dataframe.Date,
+                                                            y=dataframe[sell_ticker], mode='lines', name=sell_ticker)
+                                    sub_trace3 = go.Scatter(x=dataframe.Date,
+                                                            y=data.ratio, mode='lines', name='Ratio')
+                                    sub_trace4 = go.Scatter(x=dataframe.Date,
+                                                            y=data.spreadMA200, mode='lines', name='SpreadMA200')
+                                    sub_trace5 = go.Scatter(x=dataframe.Date,
+                                                            y=data.spread, mode='lines', name='Spread')
+                                    sub_trace6 = go.Scatter(x=dataframe.Date,
+                                                            y=data.ratioEMA, mode='lines', name='ratioEMA')
+                                    sub_trace7 = go.Scatter(x=dataframe.Date,
+                                                            y=data.ratioMin, mode='lines', name='ratioMin')
+                                    sub_trace8 = go.Scatter(x=dataframe.Date,
+                                                            y=data.ratioMax, mode='lines', name='ratioMax')
+
+                                    sub_fig = tools.make_subplots(rows=2, cols=2, subplot_titles=[
+                                        buy_ticker, 'Ratio', sell_ticker, "Spread"], shared_xaxes=True, x_title=f"Correlation: {corr_check}")
+                                    sub_fig.append_trace(sub_trace1, 1, 1)
+                                    sub_fig.append_trace(sub_trace2, 2, 1)
+                                    sub_fig.append_trace(sub_trace3, 1, 2)
+                                    sub_fig.append_trace(sub_trace4, 2, 2)
+                                    sub_fig.append_trace(sub_trace5, 2, 2)
+                                    sub_fig.append_trace(sub_trace6, 1, 2)
+                                    sub_fig.append_trace(sub_trace7, 1, 2)
+                                    sub_fig.append_trace(sub_trace8, 1, 2)
+
+                                    sub_fig.show()
 
 
-pair(all_tickers, all_tickers)
+pair('ABB', 'OMF ORA ORCL ORI OSK OSW OTEX OUT OVV OXY OZK PAAS PACW PAG PAGP PB PBF PBR PCH PDCE PDCO PEG PFE PFGC PFS PG PGR PH PHG PII PINC PK PKG PKI PLD PLNT PM PNC PNFP PNM PNR PNW POR POST POWI PRA PRGO PRQR PRU PSA PSTG PSX PVH PWR PXD QDEL QSR QTWO RAD')
